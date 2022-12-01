5c111fe0f706b82949feed7fac248a5ccd650f22
Danny <danny35009@gmail.com>
2022-12-01 23:48:16 +0200

    Homework301122.java

--- src/main/java/Homework301122.java
+++ src/main/java/Homework301122.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
 public class Homework301122 {
-    import java.util.Scanner;
 
-    public class Homework301122 {
         public static void main(String[] args) {
             System.out.println("Please, enter your name:");
+
             Scanner scanner = new Scanner(System.in);
             String name = scanner.nextLine();
+
             System.out.println("Please, enter one of your hobbies:");
             String hobby = scanner.nextLine();
+
             System.out.println("Hello, " + name + "! It is so nice to hear your hobby is " + hobby + ".");
         }
 
